7ca5a38afe253c85fa4eaf822e38c35b40a3e3f6
warithr621 <warithr21@gmail.com>
2024-05-28 13:44:05 -0500

    warith solving llll and football

---
+++ football/solve.py
@@ -0,0 +1,12 @@
+# Football
+
+N = int(input())
+for _ in range(N):
+    S = input().lower()
+    if "tony" in S:
+        if "cs" in S:
+            print("locked in")
+        else:
+            print("football")
+    else:
+        print("I'm not going to state aight")
---
+++ llll/solve.py
@@ -0,0 +1,6 @@
+# LLLL
+
+N = int(input())
+for _ in range(N):
+    A, S = map(int, input().split())
+    print("L" if A <= S else "W", end='')
