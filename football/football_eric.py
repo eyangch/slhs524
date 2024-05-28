3d0556143517345037dbdf0e38f3c6a50af7d209
eyangch <eyangch@gmail.com>
2024-05-28 14:50:09 -0500

    finish football

---
+++ football/football_eric.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+T = int(input())
+
+for i in range(T):
+	urmom = input().lower()
+	if "tony" in urmom:
+		if "cs" in urmom:
+			print("locked in")
+		else:
+			print("football")
+	else:
+		print("I'm not going to state aight")
---
+++ football/generator.py
@@ -0,0 +1,6 @@
+N = 4
+print(N)
+print("isjdoifjsoeishoetONysoeithsoo")
+print("oahgepawe9r8yaor7a238cr72n38rcp9w8cscscscscs")
+print("csttooooonny")
+print("CCCCCCCCCCCCCCCCCTCSTOnnntonY")
