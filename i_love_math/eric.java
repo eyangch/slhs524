20d1b7e551850b2189c23058b7470d120aa44f1f
eyangch <eyangch@gmail.com>
2024-05-30 12:49:22 -0500

    faster math

--- i_love_math/eric.java
+++ i_love_math/eric.java
@@ -2,40 +2,43 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
+//210222120001110000210201012001011012201120201112002000101
+//210222120001110000210201012001011012201120201112002000101
+
+//1111202111022020201111102100020212100110222100010220010222221101120200101010211110021020222222222202220200022110210221000211110110202111210202201120200211201011110021011011222110101210010111112120122222211200120100102221202201201021102211200222011102102021110012002120121011220222202221222020222
+//1111202111022020201111102100020212100110222100010220010222221101120200101010211110021020222222222202220200022110210221000211110110202111210202201120200211201011110021011011222110101210010111112120122222211200120100102221202201201021102211200222011102102021110012002120121011220222202221222020222
+
 public class eric{
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static PrintWriter pw = new PrintWriter(System.out);
 	static StringTokenizer st = new StringTokenizer("");
 	static BigInteger THREE = new BigInteger("3");
-	static int[] convert3(BigInteger x, int order){
+	static int zt[];
+	static void convert3(BigInteger x, int order, int s, int e){
 		if(order == 0){
-			int[] ret = new int[1];
-			ret[0] = x.intValue();
-			return ret;
+			zt[s] = x.intValue();
+			return;
 		}
 		BigInteger sep = THREE.pow(order);
 		BigInteger hi = x.divide(sep);
 		BigInteger lo = x.subtract(hi.multiply(sep));
-		int[] a = convert3(hi, order/2), b = convert3(lo, order/2);
-		int[] res = new int[a.length+b.length];
-		for(int i = 0; i < a.length; i++){
-			res[i] = a[i];
-		}
-		for(int i = 0; i < b.length; i++){
-			res[i+a.length] = b[i];
-		}
-		return res;
+		convert3(hi, order/2, s, (s+e)/2);
+		convert3(lo, order/2, (s+e)/2, e);
 	}
 	public static void main(String[] args) throws IOException{
+		long start = System.nanoTime();
 		String S = br.readLine();
 		BigInteger Z = new BigInteger(S);
+		Z = BigInteger.TEN.pow(100000);
 		int l10 = S.length();
+		l10 = 100001;
 		int l3 = (int)(l10 * 2.095903274289384604296567522021401250607518006)+5;
 		int rl3 = 1;
 		while(rl3 < l3){
 			rl3 <<= 1;
 		}
-		int[] zt = convert3(Z, rl3);
+		zt = new int[2*rl3];
+		convert3(Z, rl3, 0, 2*rl3);
 		int l0 = 0;
 		for(int i = 0; i < zt.length; i++){
 			if(zt[i] != 0) break;
@@ -45,10 +48,12 @@ public class eric{
 		for(int i = 0; i < z.length; i++){
 			z[i] = zt[i+l0];
 		}
-		for(int i = 0; i < z.length; i++){
+		/*for(int i = 0; i < z.length; i++){
 			pw.print(z[i]);
 		}
-		pw.println();
+		pw.println();*/
+		long end = System.nanoTime();
+		pw.println((end - start) / 1000000);
 		br.close(); pw.close();
 	}
 	static String next() throws IOException{
